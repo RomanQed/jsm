0bfa94a8193d09f068fba62c8dcb2daf765dd83f
RomanQed <gbakaldin@gmail.com>
2024-01-20 14:49:35 +0300

    Remove unused constructor

--- src/main/java/com/github/romanqed/jsm/bytecode/LocalVariablesWriter.java
+++ src/main/java/com/github/romanqed/jsm/bytecode/LocalVariablesWriter.java
@@ -1,15 +1,10 @@
 package com.github.romanqed.jsm.bytecode;
 
-import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 
 class LocalVariablesWriter extends ClassWriter {
     public LocalVariablesWriter(int flags) {
         super(flags);
-    }
-
-    public LocalVariablesWriter(ClassReader classReader, int flags) {
-        super(classReader, flags);
     }
 
     public LocalVariablesSorter visitMethodWithLocals(final int access,
