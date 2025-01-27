99d415cb6de2e57943b686a4e9645e41ab8d0422
Roman Bakaldin <gbakaldin@gmail.com>
2025-01-27 22:21:03 +0300

    Add string comparator

---
+++ switchgen/src/main/java/com/github/romanqed/switchgen/StringComparator.java
@@ -0,0 +1,34 @@
+package com.github.romanqed.switchgen;
+
+import org.objectweb.asm.Label;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+import java.util.function.Consumer;
+
+public final class StringComparator implements Comparator {
+
+    @Override
+    public void compare(MethodVisitor visitor, Object expected, Consumer<MethodVisitor> then) {
+        // Stack: actualStringRef
+        visitor.visitLdcInsn(expected);
+        // Stack: actualStringRef, expectedStringRef
+        // Swap -> expectedStringRef, actualStringRef
+        visitor.visitInsn(Opcodes.SWAP);
+        // Use invoke special instead of virtual because
+        // String is final class, and we want to call directly String#equals
+        visitor.visitMethodInsn(
+                Opcodes.INVOKESPECIAL,
+                "java/lang/String",
+                "equals",
+                "(Ljava/lang/Object;)Z",
+                false
+        );
+        var exit = new Label();
+        // Jump out if comparison result is false (0)
+        visitor.visitJumpInsn(Opcodes.IFEQ, exit); // if (expected.equals(actual)) {
+        then.accept(visitor); // then();
+        // }
+        visitor.visitLabel(exit);
+    }
+}
